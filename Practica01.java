b90f0a27f45a1cb368836d485d89ab33f201a1c8
eacg91 <eacg91@eacg91-Lenovo-G460.(none)>
2013-03-06 10:36:59 -0600

    separa lineas y diferencia entre CODOP etiqueta y operando

--- Practica01.java
+++ Practica01.java
@@ -1,5 +1,5 @@
 /**
- * Taller de Programacion Orientada a Objetos
+ * Taller de Programacion de Sistemas
  * @author Edwin Alberto Castañeda García
  */
 import java.io.*;
@@ -13,20 +13,43 @@ public class Practica01 {
 	public String QuitarComentarios(String linea){
 		if(linea.contains(";")){
 			int p=linea.indexOf(';');
-			if(p>0){
-				linea=linea.substring(0,p);
-			}else if(p==0)
-				linea="";
+			linea=linea.substring(0,p);
 		}
 		return linea;
     }
+	public String diferenciarTokens(String linea){
+		int flag=0;
+		StringTokenizer st=new StringTokenizer(linea);
+		String sToken="";
+		char pCt;//,pCl=linea.charAt(0);
+		System.out.print("\n   "+nLineas);//Cuenta numero de lineas leidas
+		while(st.hasMoreTokens()){
+			sToken=st.nextToken();//Tomando token de la linea
+			pCt=sToken.charAt(0);//Primer caracter del token
+			if(flag==0)
+				System.out.print("\t");
+			else //Si hay Bandera, quitarla
+				flag=0;
+			if(Character.isLetter(pCt) && sToken.length()<=8){
+				if(sToken.length()<=5 && pCt!=linea.charAt(0) && sToken.indexOf('.')==sToken.lastIndexOf('.')){
+					System.out.print("\t\t  "+sToken);//CODOP
+				}else if(sToken.length()>5 | pCt==linea.charAt(0) ){
+					System.out.print(sToken);//Etiqueta
+				}
+					flag=1;
+			}else{
+				System.out.print("\t\t "+sToken);//Operando
+			}
+		};
+		return linea;
+    }
 /*************************/
-	public void escribir(String ruta, String cadena){
+	public void escribir(String ruta){
 		File archivo = new File(ruta+".INST");
 		try {
 			FileWriter escribirArchivo = new FileWriter(archivo, true);
 			BufferedWriter buffer = new BufferedWriter(escribirArchivo);
-			buffer.write(cadena);
+			buffer.write(retorno);
 			buffer.newLine();
 			buffer.close();
 		}
@@ -39,32 +62,26 @@ public class Practica01 {
 		String linea,extension;
 		linea=retorno="";
 		nLineas=p=0;
-		extension=ruta.substring(ruta.lastIndexOf('.'),ruta.length());
 		File archivo = new File(ruta);
 		try {
 			FileReader leerArchivo = new FileReader(archivo);
 			BufferedReader buffer = new BufferedReader(leerArchivo);
+			System.out.print("#linea\t Etiqueta\t CODOP\t\t Operando");
 			while ((linea = buffer.readLine()) != null){
-				nLineas++;
-				if(linea.contains(";")){
-					p=linea.indexOf(';');
-					if(p>0){
-						linea=linea.substring(0,p);
-					}else if(p==0)
-						linea="";
-				}
+				linea=QuitarComentarios(linea); //Quitar comentarios
+				linea=diferenciarTokens(linea.toString());
 				retorno+=linea+"\n";
+				nLineas++;
 			}
 			buffer.close();
-			System.out.println("/***************/\n"+retorno+"/***************/");
+			escribir(ruta);
 		}
 		catch (Exception ex){
-			/*** Solucion generica ***/
+			extension=ruta.substring(ruta.lastIndexOf('.'),ruta.length());
 			if(extension.equals(".ASM")){
 				p=ruta.lastIndexOf('.');
 				leer(ruta.replace(extension,".asm"));
 			}else
-			/*** FIN de Solucion generica ***/
 				System.out.println("NO EXISTE ARCHIVO "+ruta);
 		}
 	}
@@ -72,20 +89,20 @@ public class Practica01 {
     public static void main(String a[]){
     	Scanner Lectora=new Scanner(System.in);
     	Practica01 archivo=new Practica01();
-    	String ruta,extension=".ASM";
+    	String ruta,extension=".asm";
 		int opcion;
 		do{
 			opcion=0;
 		    System.out.print("\n Por Favor, escriba ruta con archivo:   ");
-		    ruta=Lectora.nextLine();
-			if(ruta.contains(extension)|ruta.contains(extension.toLowerCase()) ){
+		    ruta=Lectora.next();
+			if(ruta.contains(extension.toUpperCase())|ruta.contains(extension.toLowerCase()) ){
 				archivo.leer(ruta);
 			}else if(!ruta.contains(".")){
 				System.out.println("\n No hay punto \".\" en el nombre, se abrira "+ruta+extension);
 				archivo.leer(ruta+=extension);
 			}else
 				System.out.println("\n ERROR! Solo se admiten archivos "+extension);
-			System.out.println(" Desea reutilizar el programa? (0=NO,1=SI):");
+			System.out.print("\n Desea reutilizar el programa? (0=NO,1=SI): ");
 			opcion=Lectora.nextInt();
 		}while(opcion==1);
     }
