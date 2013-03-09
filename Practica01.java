d0728b9367c2ee71e5d1243cd3e881957f96d548
eacg91 <eacg91@eacg91-Lenovo-G460.(none)>
2013-03-09 11:26:13 -0600

    corregido funcion escribir

--- Practica01.java
+++ Practica01.java
@@ -6,25 +6,29 @@ import java.io.*;
 import java.util.*;
 import java.lang.*;
 public class Practica01 {
-	String retorno="",original="",log="";
-	int p=0,nLineas=0;
+	String original="",retorno="",log="";
+	int nLineas=0;
 	Scanner Lectora=new Scanner (System.in);
 /*************************/
-	public void escribirlog(/*String ruta*/){
-		//int p=ruta.lastIndexOf('.');
-		//File revisando = new File(ruta.substring(0,p)+".INST");
-		//Lo anterior es por lo pronto inecesario
-		File resultado = new File("log.log");
-		try {
-			FileWriter escribirArchivo = new FileWriter(resultado, true);
-			BufferedWriter buffer = new BufferedWriter(escribirArchivo);
-			buffer.write(log);
-			buffer.newLine();
-			buffer.close();
-		}
-		catch (Exception ex) {
-		}
+public void mostrar(String ruta){
+	int p=0;
+	File archivo = new File(ruta);
+	if(archivo.exists()){
+		p=ruta.lastIndexOf('.');
+		System.out.println("\n\t---> Original de "+ruta+"\n"+original);
+		System.out.println("\n\t---> Retorno en .INST \n"+retorno);
+		escribir(ruta.substring(0,p)+".INST",retorno);
+		/***** Ultimas revisiones de errores *****/
+		if(retorno.lastIndexOf("END")==0)
+		   log+="\n No finaliza con END";
+		escribir("log.log",log);
+		/***** *****/
+		if(log.length()>0)
+			System.out.println("\n\t---> ERRORES! \n"+log);
+		else
+			System.out.println("\n\t Completado Sin Errores");			
 	}
+}
 /*************************/
 	public String QuitarComentarios(String linea){
 		if(linea.contains(";")){
@@ -71,14 +75,19 @@ public class Practica01 {
 		return linea2;
     }
 /*************************/
-	public void escribir(String ruta){
-		int p=ruta.lastIndexOf('.');
-		String nuevaRuta=ruta.substring(0,p)+".INST";
+	public void escribir(String nuevaRuta,String contenido){
 		File archivo = new File(nuevaRuta);
+		if(archivo.exists()){
+			if(archivo.delete()){
+			    System.out.println("\n Reescribiendo "+nuevaRuta+"...");
+			}else
+			    System.out.println("\n Error al reescribir "+nuevaRuta+"...");
+		}else
+			System.out.println("\n Creando "+nuevaRuta+"...");
 		try {
 			FileWriter escribirArchivo = new FileWriter(archivo, true);
 			BufferedWriter buffer = new BufferedWriter(escribirArchivo);
-			buffer.write(retorno);
+			buffer.write(contenido);
 			buffer.newLine();
 			buffer.close();
 		}
@@ -86,16 +95,27 @@ public class Practica01 {
 		}
 	}
 /*************************/
-	public void leer(String ruta){
+	public String leer(String ruta){
 		ArrayList array = new ArrayList();
 		String linea,extension;
+		int p=ruta.lastIndexOf('.');
 		linea=retorno=original="";
-		nLineas=p=0;
+		nLineas=0;
+		extension=ruta.substring(p,ruta.length());
 		File archivo = new File(ruta);
+		if(!archivo.exists()){
+			if(ruta.contains(extension.toUpperCase()) ){
+				ruta=ruta.substring(0,p)+extension.toLowerCase();
+				archivo = new File(ruta);
+			}else if(ruta.contains(ruta.toLowerCase()) ){
+				ruta=ruta.substring(0,p)+extension.toUpperCase();
+				archivo = new File(ruta);
+			}
+		}
 		try {
 			FileReader leerArchivo = new FileReader(archivo);
 			BufferedReader buffer = new BufferedReader(leerArchivo);
-			retorno=("#linea\tEtiqueta\tOPeration CODe\t\tOperando");
+			retorno=("#linea\tEtiqueta\tCODOP \t\tOperando");
 			while ((linea = buffer.readLine()) != null){
 				original+=linea+"\n";
 				linea=QuitarComentarios(linea); //Quitar comentarios
@@ -105,28 +125,12 @@ public class Practica01 {
 					retorno+=linea;
 				}
 			}
-			/***** Ultimas revisiones de errores *****/
-			if(retorno.lastIndexOf("END")==0)
-			   log+="\n No finaliza con END";
-			/***** *****/
 			buffer.close();
-			System.out.println("\n\t---> Original de "+ruta+"\n"+original);
-			System.out.println("\n\t---> Retorno en .INST \n"+retorno);
-			escribir(ruta);
-			escribirlog();
-			if(log.length()>0)
-				System.out.println("\n\t---> ERRORES! \n"+log);
-			else
-				System.out.println("\n\t Completado Sin Errores \n");
 		}
 		catch (Exception ex){
-			extension=ruta.substring(ruta.lastIndexOf('.'),ruta.length());
-			if(extension.equals(".ASM")){
-				p=ruta.lastIndexOf('.');
-				leer(ruta.replace(extension,".asm"));
-			}else
-				System.out.println("NO EXISTE ARCHIVO "+ruta);
+			System.out.println("NO EXISTE ARCHIVO "+ruta);
 		}
+		return ruta;
 	}
 /*************************/
     public static void main(String a[]){
@@ -138,13 +142,16 @@ public class Practica01 {
 			opcion=0;
 		    System.out.print("\n Por Favor, escriba ruta con archivo:   ");
 		    ruta=Lectora.next();
-			if(ruta.contains(extension.toUpperCase())|ruta.contains(extension.toLowerCase()) ){
-				archivo.leer(ruta);
-			}else if(!ruta.contains(".")){
+			if(!ruta.contains(".")){
 				System.out.println("\n No hay punto \".\" en el nombre, se abrira "+ruta+extension);
-				archivo.leer(ruta+=extension);
+				ruta+=extension;
+			}
+			if(ruta.contains(extension.toUpperCase())|ruta.contains(extension.toLowerCase()) ){
+				ruta=archivo.leer(ruta);
+				archivo.mostrar(ruta);
 			}else
 				System.out.println("\n ERROR! Solo se admiten archivos "+extension);
+			
 			System.out.print("\n Desea reutilizar el programa? (0=NO,1=SI): ");
 			opcion=Lectora.nextInt();
 		}while(opcion==1);
